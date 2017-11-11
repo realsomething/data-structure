746b495c61d72643e394c3f5ff504736ac34eb4c
sky <sky@skydeiMac.local>
2017-11-11 22:15:07 +0800

    SeqList.

---
+++ SeqList/cmake-build-debug/CMakeFiles/SeqList.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SeqList.dir/main.cpp.o"
+  "SeqList.pdb"
+  "SeqList"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SeqList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ SeqList/cmake-build-debug/CMakeFiles/SeqList.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SeqList.
+# This may be replaced when dependencies are built.
