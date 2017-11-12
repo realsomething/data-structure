11ccfe652050273f97b9b16b8f64bcfc8805555c
“sky” <hsf1002@gmail.com>
2017-11-12 08:52:39 +0800

    SeqList.

---
+++ SeqList/SeqList.h
@@ -0,0 +1,159 @@
+//
+// Created by  sky on 2017/11/12.
+//
+
+#ifndef SEQLIST_SEQLIST_H
+#define SEQLIST_SEQLIST_H
+
+#endif //SEQLIST_SEQLIST_H
+
+
+class SeqList
+{
+private:
+    DataType list[MAX_SIZE];
+    int size;
+
+public:
+    // init
+    void ListInitiate()
+    {
+        cout << "list initializing..." << endl;
+        size = 0;
+    }
+
+    // insert x before i,    0 <= i <= L->size
+    bool ListInsert( int i, DataType x)
+    {
+        if (size >= MAX_SIZE)
+        {
+            cout << "list is full." << endl;
+            return false;
+        }
+        else if (i < 0 || i > size)
+        {
+            cout << "list insert param is invalid." << endl;
+            return false;
+        }
+        else
+        {
+            for (int j=size; j>i; j--)
+            {
+                list[j] = list[j-1];
+            }
+
+            list[i] = x;
+            size++;
+            cout << "list insert data success." << endl;
+            return true;
+        }
+    }
+
+    // delete x from i,     0 <= i <= L->size - 1
+    bool ListDelete(int i, DataType *x)
+    {
+        if (size == 0)
+        {
+            cout << "list is empty." << endl;
+            return false;
+        }
+        else if (i < 0 || i > size - 1)
+        {
+            cout << "list delete param is invalid." << endl;
+            return false;
+        }
+        else
+        {
+            *x = list[i];
+
+            for (int j=i+1; j<size; j++)
+            {
+                list[j-1] = list[j];
+            }
+
+            size--;
+            cout << "list delete data success."  << endl;
+            return true;
+        }
+    }
+
+
+    // modify original value in i with x
+    bool ListModify(int i, DataType x)
+    {
+        if (size == 0)
+        {
+            cout << "list is empty." << endl;
+            return false;
+        }
+        else if (i < 0 || i > size - 1)
+        {
+            cout << "list modify param is invalid." << endl;
+            return false;
+        }
+        else
+        {
+            list[i] = x;
+
+            cout << "list modify data success." << endl;
+            return true;
+        }
+    }
+
+    // search value in L with x
+    bool ListSearch(int i, DataType x)
+    {
+        if (size == 0)
+        {
+            cout << "list is empty." << endl;
+            return false;
+        }
+        else if (i < 0 || i > size - 1)
+        {
+            cout << "list search param is invalid." << endl;
+            return false;
+        }
+        else
+        {
+            int j=0;
+
+            while (j < size)
+            {
+                if (list[j] == x)
+                {
+                    cout << "list search data success." << endl;
+                    return true;
+                }
+                j++;
+            }
+            cout << "list search data failed." << endl;
+            return false;
+        }
+    }
+
+    // print the list
+    void ListPrint()
+    {
+        cout << "list data showing..................." << endl;
+        if (size == 0)
+        {
+            cout << "list is empty.";
+        }
+        else
+        {
+            cout << "list size[" << size << "]:  ";
+
+            for (int i=0; i< size; i++)
+            {
+                cout << list[i] << '\t';
+            }
+            cout << endl;
+        }
+    }
+
+    int ListSize()
+    {
+        return size;
+    }
+
+};
--- SeqList/main.cpp
+++ SeqList/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+using namespace std;
+
+
+#define MAX_SIZE 100
+typedef int DataType;
+
+#include "SeqList.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    SeqList *seqList = new SeqList();
+
+    seqList->ListInitiate();
+
+    for (int i=0; i<10; i++)
+    {
+        seqList->ListInsert(i, rand() % 100);
+    }
+
+    seqList->ListPrint();
+
+    DataType  x = 0;
+    seqList->ListDelete(0, &x);                         // delete the first
+    seqList->ListPrint();
+
+    seqList->ListDelete(seqList->ListSize()-1, &x);     // delete the last
+    seqList->ListPrint();
+    
     return 0;
 }
