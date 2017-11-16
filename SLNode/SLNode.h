dd57be4cf0d02aba51565878db56110adc83068c
“sky” <hsf1002@gmail.com>
2017-11-16 23:07:00 +0800

    SLNode.

---
+++ SLNode/SLNode.h
@@ -0,0 +1,165 @@
+//
+// Created by  sky on 2017/11/12.
+//
+
+#ifndef SLNODE_SLNODE_H
+#define SLNODE_SLNODE_H
+
+#endif //SLNODE_SLNODE_H
+
+
+struct Node
+{
+    DataType data;
+    Node *next;
+    Node(const DataType &d):data(d), next(NULL)
+    {
+
+    }
+
+    Node()
+    {
+        data = 0;
+        next = NULL;        // of vital importance, in head insert list, ends the link list
+    }
+};
+
+class SLinkList
+{
+private:
+    Node *head;
+    int size;
+
+public:
+
+    SLinkList()
+    {
+        head = new Node();      // with head node
+    }
+
+    virtual ~SLinkList()
+    {
+        Node *p = head;
+        Node *q = head;
+
+        while (p != NULL)
+        {
+            q = p;
+            p = p->next;
+            delete  q;//free(q);
+        }
+    }
+
+    void create_list_head()
+    {
+        for (int i=0; i<10; ++i)
+        {
+            insert_pos(rand() % 100, 0);
+        }
+    }
+
+    bool insert_pos(DataType x, int position)
+    {
+        Node *p = head;
+        Node *q = NULL;
+
+        int j = -1;
+
+        // point to the location before position
+        while (p->next != NULL && j < position - 1)
+        {
+            p = p->next;
+            j++;
+        }
+
+        if (j != position - 1)
+        {
+            cout << "SLinkList insert position error." << endl;
+            return false;
+        }
+
+        q = new Node(x);
+        q->next = p->next;
+        p->next = q;
+        cout << "SLinkList insert data success." << endl;
+
+        return true;
+    }
+
+    bool delete_pos(DataType &x, int position)
+    {
+        Node *p = head;
+        Node *q = NULL;
+
+        int j = -1;
+
+        // point to the location before position, make sure the position not empty, has the real data
+        while (p->next != NULL && p->next->next != NULL & j < position - 1)
+        {
+            p = p->next;
+            j++;
+        }
+
+        if (j != position - 1)
+        {
+            cout << "SLinkList delete position error." << endl;
+            return false;
+        }
+
+        q = p->next;
+        x = q->data;
+        p->next = p->next->next;
+        delete(q);
+        cout << "SLinkList delete data success." << endl;
+
+        return true;
+    }
+
+    Node* get_node(int position)
+    {
+        Node *p = head;
+        int j = -1;
+
+        while (p->next != NULL && j < position)
+        {
+            p = p->next;
+            j++;
+        }
+
+        if (j != position )
+        {
+            cout << "SLinkList get position error." << endl;
+            return NULL;
+        }
+
+        return p;
+    }
+
+    int get_size()
+    {
+        Node *p = head;
+        size = 0;
+
+        while (p->next != NULL)
+        {
+            size++;
+            p = p->next;
+        }
+
+        return size;
+    }
+
+    void print_linklist()
+    {
+        Node *p = head;
+
+        while (p->next != NULL)
+        {
+            cout << p->next->data << '\t';
+            p = p->next;
+        }
+
+        cout << endl;
+    }
+
+};
---
+++ SLNode/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+
+typedef int DataType;
+
+#include "SLNode.h"
+
+int main() {
+
+    SLinkList sLinkList = SLinkList();
+    DataType x = 0;
+    Node *p = NULL;
+
+    sLinkList.create_list_head();
+
+    sLinkList.print_linklist();
+
+    sLinkList.delete_pos(x, 0);                         // delete the first
+
+    sLinkList.print_linklist();
+
+    sLinkList.delete_pos(x, sLinkList.get_size() - 1);  // delete the last
+
+    sLinkList.print_linklist();
+
+    p = sLinkList.get_node(0);                                  // get the first
+    cout << "first node: " << p->data << endl;
+
+    p = sLinkList.get_node(sLinkList.get_size() - 1);           // get the last
+    cout << "last node: " << p->data << endl;
+
+    return 0;
+}
