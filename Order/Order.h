99712b8fb2f01d49fe9985e2c84219a83d5bf40a
sky <hsf1002@gmail.com>
2017-12-05 08:30:15 +0800

    order shell.

--- Order/Order.h
+++ Order/Order.h
@@ -38,6 +38,9 @@ public:
         cout << endl;
     }
 
+    /* insert directly
+     * 进行n-1轮，假设第一个元素已排好序，从第二个元素开始，顺序的把待排序的元素按其关键字大小插入到已经排好序的子集合的适当位置
+     * */
     void insert_sort()
     {
         for (int i=0; i<size-1; ++i)
@@ -54,6 +57,37 @@ public:
         }
     }
 
+    /* first group, then insert directly in each group
+     * d[]: number of each group's member
+     * num_of_d: number of group
+     * */
+    void shell_sort(int d[], int num_of_d)
+    {
+        for (int m=0; m<num_of_d; ++m)
+        {
+            int span = d[m];
+
+            for (int k=0; k<span; ++k)
+            {
+                for (int i=k; i<size-span; i=i+span)
+                {
+                    DataType  temp = array[i+span];
+                    int j=i;
+
+                    while (j > -1 && temp < array[j])
+                    {
+                        array[j+span] = array[j];
+                        j -= span;
+                    }
+                    array[j+span] = temp;
+                }
+            }
+        }
+    }
+
+    /* 从待排序的数据元素集合中选取关键字最小的元素并与原始数据元素集合中的第一个元素进行交换，然后从不包含第一个位置的数据元素集合中选取
+     * 关键字最小的数据元素并与原始数据集合中第二个元素交换位置，直到待排序集合中只剩下一个元素位置
+     * */
     void select_sort()
     {
         int i;
--- Order/main.cpp
+++ Order/main.cpp
@@ -9,12 +9,13 @@ typedef int DataType;
 int main() {
 
     Order order = Order();
+    int d[3] = {5, 3, 1};
 
     order.print_data();
 
     //order.select_sort();
-
-    order.insert_sort();
+    //order.insert_sort();
+    order.shell_sort(d, sizeof(d)/sizeof(int));
 
     order.print_data();
 
